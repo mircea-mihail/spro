4541d143cf0811e75ae40b599bde92a79f8c2fa4
Ionescu Mircea Mihail <mihail.mircea12@gmail.com>
2023-01-29 13:26:36 +0200

    added functionality to -t to allow you to pick the date to display the table for

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ void usage(){
     std::cout << "Bash tool to track your progress while studying\n\n";
 
     std::cout << "Options:\n";
-    std::cout << "    -s, --start <TITLE>    starts the timer and sets the session title as TITLE\n";
-    std::cout << "    -e, --end              stops the timer\n";
-    std::cout << "    -t, --table            prints the progress table for the day\n";
-    std::cout << "    -b, --balance <DAY>    prints the total progress of a given day (format: 11Jan2022)\n";
-    std::cout << "    -c, --current          prints the current progress (how long has it been since -s without using -e)\n";
+    std::cout << "    -s, --start <TITLE>   starts the timer and sets the session title as TITLE\n";
+    std::cout << "    -e, --end             stops the timer\n";
+    std::cout << "    -t, --table <DAY>     prints the progress table for the day <DAY> is optional\n";
+    std::cout << "                          if no <DAY> is given, the current day is implied\n";
+    std::cout << "    -b, --balance <DAY>   prints the total progress of a given day (format: 11Jan2022)\n";
+    std::cout << "                          if no <DAY> is given, the current day is implied\n";
+    std::cout << "    -c, --current         prints the current progress (how long has it been since -s without using -e)\n";
 
 }
 
@@ -325,8 +327,24 @@ void options(int argc, char* argv[]){
     }
 
     if(tflag == 1){
-        time_t now = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now());
-        printTable(generateFilename(now));
+        if(argc == 2){
+            time_t now = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now());
+            printTable(generateFilename(now));
+        }
+        else if(argc > 3){
+            std::cout << "Only one date at a time!\n\n";
+            usage();
+            exit(EXIT_FAILURE);
+        }
+        else{
+            std::string inputDate = argv[2];
+            if(existsFile(inputDate)){
+                printTable(inputDate);
+            }
+            else{
+                std::cout << "No data for the given date (" << inputDate << ")\n";
+            }
+        }
     }
 
     if(bflag == 1){
